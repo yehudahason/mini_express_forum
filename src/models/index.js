6dba161b598ba626dc7be0f9977f06387950880f
Yehuda Hason <yehudaxt@gmail.com>
2025-12-07 02:07:24 +0200

    compose file

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
+import "dotenv/config";
 
 export const sequelize = new Sequelize(
-  //"mini_forum",
   process.env.PGDATABASE,
-  // "user",
+
   process.env.PGUSER,
-  // "password",
+
   process.env.PGPASSWORD,
   {
     host: process.env.PGHOST,
