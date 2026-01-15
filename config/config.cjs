c3e7d9bca27e8594699c786147a4ba7f55e9d7bd
Yehuda Hason <yehudaxt@gmail.com>
2026-01-15 13:43:06 +0200

    imoji

--- config/config.cjs
+++ config/config.cjs
@@ -2,18 +2,18 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "user",
-    password: process.env.DB_PASSWORD || "jf2zypp99",
-    database: process.env.DB_NAME || "forum",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 
   production: {
-    username: process.env.DB_USER || "user",
-    password: process.env.DB_PASSWORD || "jf2zypp99",
-    database: process.env.DB_NAME || "forum",
-    host: process.env.DB_HOST || "127.0.0.1",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
   },
 };
