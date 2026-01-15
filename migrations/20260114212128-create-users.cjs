e34d15465b9397210af87ede6188d63d0778320f
Yehuda Hason <yehudaxt@gmail.com>
2026-01-15 13:51:34 +0200

    Revert "Delete migrations directory"
    This reverts commit 57267f46aaec627c8acf28db86cdd5effb6ecafc.

---
+++ migrations/20260114212128-create-users.cjs
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("Users");
+  },
+};
